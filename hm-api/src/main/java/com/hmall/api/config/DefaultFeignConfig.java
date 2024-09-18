560153abe0a7ec6a673e02c34c67db367ef4aeda
cheng <928720204@qq.com>
2024-09-18 14:14:30 +0800

    feat:OpenFeign在服务间传递用户信息

--- hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
+++ hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
@@ -1,6 +1,8 @@
 package com.hmall.api.config;
 
+import com.hmall.common.utils.UserContext;
 import feign.Logger;
+import feign.RequestInterceptor;
 import org.springframework.context.annotation.Bean;
 
 /**
@@ -11,5 +13,18 @@ public class DefaultFeignConfig {
     @Bean
     public Logger.Level feignLoggerLevel() {
         return Logger.Level.FULL;
+    }
+
+    @Bean
+    public RequestInterceptor userInfoRequestInterceptor() {
+        return new RequestInterceptor(){
+            @Override
+            public void apply(feign.RequestTemplate requestTemplate) {
+                Long userId = UserContext.getUser();
+                if (userId != null) {
+                    requestTemplate.header("user-info", userId.toString());
+                }
+            }
+        };
     }
 }
