f832fdb4fc714d7355b38cb2d27d3753990af32d
cheng <928720204@qq.com>
2024-09-19 14:25:09 +0800

    feat:针对feign的远程调用进行流量控制（fallback）

--- hm-api/src/main/java/com/hmall/api/client/ItemClient.java
+++ hm-api/src/main/java/com/hmall/api/client/ItemClient.java
@@ -1,5 +1,6 @@
 package com.hmall.api.client;
 
+import com.hmall.api.client.fallback.ItemClientFallbackFactory;
 import com.hmall.api.dto.ItemDTO;
 import com.hmall.api.dto.OrderDetailDTO;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -15,7 +16,7 @@ import java.util.List;
  * @Author Cheng
  * @Date 2024 09 15 00 09
  **/
-@FeignClient("item-service")
+@FeignClient(value = "item-service",fallbackFactory = ItemClientFallbackFactory.class)
 public interface ItemClient {
     @GetMapping("/items")
     List<ItemDTO> queryItemByIds(@RequestParam("ids") Collection<Long> ids);
